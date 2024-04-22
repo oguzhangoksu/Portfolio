ed46b6d00c3277cf5c3ef58cca7c9cf48538142d
Oğuzhan Göksu <oguzhan.goksu@std.antalya.edu.tr>
2024-04-22 14:51:23 +0300

    revised portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./components/About.js";
 import Resume from"./components/Resume.js";
 import Footer from "./components/Footer.js";
 import {
-  HashRouter as Router,
   Route,
   Routes,
   Navigate
@@ -14,20 +13,20 @@ import {
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/Resume" element={<Home/>} />
-          <Route path="/Resume/about" element={<About/>} />
-          <Route path="/Resume/project" element={<Projects/>} />
-          <Route path="/Resume/resume" element={<Resume/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/project" element={<Projects/>} />
+          <Route path="/resume" element={<Resume/>} />
           <Route path="*" element={<Navigate to="/Resume"/>} />
           
         </Routes>
         <Footer/>
       </div>
-    </Router>
+    
     
   );
 }
